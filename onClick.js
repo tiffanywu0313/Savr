b6c93356f26d7f9faa666c489a3960f7f4c70923
dianavolt <diana.voltchok@telus.com>
2019-05-04 16:01:24 -0700

    feat(mainscreen): add second screen link

--- onClick.js
+++ onClick.js
@@ -1,3 +1,7 @@
-document.getElementById('main-button').addEventListener('click', function () {
-  // change screen
+document.getElementById('dashboard-button').addEventListener('click', function () {
+  window.location.href  = './trip.html'
 });
+
+// document.getElementById("dashboard-button'").onclick = function () {
+//   window.location.href = "www.google.com";
+// };
